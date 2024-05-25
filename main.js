9c04d51a954db147edd45f73d2375c308f8b84d0
aatishsang <149300870+aatishsang@users.noreply.github.com>
2024-05-25 15:54:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+mustacheX=0;
+mustacheY=0;
+
+function preload() {
+    clown_nose = loadImage('https://i.postimg.cc/FRvhW4fD/download-1-removebg-preview.png');
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log('PoseNet Is Initialized');
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    image(clown_nose, mustacheX +130, mustacheY +130, 60, 60);
+}
+
+function take_snapshot(){
+    save('myFilterImage.png');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+        {
+            console.log(results);
+            noseX = results[0].pose.nose.x;
+            noseY = results[0].pose.nose.y;
+            console.log("nose x = " + noseX);
+            console.log("nose y = " + noseY);
+        }
+}
